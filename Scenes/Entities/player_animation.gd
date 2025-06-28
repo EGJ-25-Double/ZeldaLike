c0abf0fd3dd35180ae515f20ec23fc50c8d64e1c
Julien Di Natale <avoltance@hotmail.fr>
2025-06-28 23:06:45 +0200

    Rework direction check

--- Scenes/Entities/player_animation.gd
+++ Scenes/Entities/player_animation.gd
@@ -2,23 +2,25 @@ extends AnimatedSprite2D
 
 class_name CharacterAnimation
 
-const MOVEMENT_TO_IDLE = {
-	"walk_down": "idle_down",
-	"walk_up": "idle_up",
-	"walk_left": "idle_left",
-	"walk_right": "idle_right"
+const DIR_TO_IDLE = {
+	Direction.Down: "idle_down",
+	Direction.Up: "idle_up",
+	Direction.Left: "idle_left",
+	Direction.Right: "idle_right"
 }
 
-func play_movement_animation(velocity: Vector2):
-	if velocity.y > 0:
-		play("walk_down")
-	elif velocity.y < 0:
-		play("walk_up")
-	elif velocity.x > 0:
-		play("walk_right")
-	elif velocity.x < 0:
-		play("walk_left")
+const DIR_TO_MOVEMENT = {
+	Direction.Down: "walk_down",
+	Direction.Up: "walk_up",
+	Direction.Left: "walk_left",
+	Direction.Right: "walk_right"
+}
+
+func play_movement_animation():
+	if DIR_TO_MOVEMENT.keys().has(PlayerUtils.player_dir):
+		play(DIR_TO_MOVEMENT[PlayerUtils.player_dir])
+
 		
 func play_idle_animation():
-	if MOVEMENT_TO_IDLE.keys().has(animation):
-		play(MOVEMENT_TO_IDLE[animation])
+	if DIR_TO_IDLE.keys().has(PlayerUtils.player_dir):
+		play(DIR_TO_IDLE[PlayerUtils.player_dir])
---
+++ Scenes/direction.gd
@@ -0,0 +1,9 @@
+class_name Direction
+enum
+{
+	None = 0,
+	Left = 1 << 0,
+	Up = 1 << 1,
+	Right = 1 << 2,
+	Down = 1 << 3
+}
