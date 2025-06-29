35aac08a430d27fdf394bf7f0547944cc61606be
Thomas Rogulski <rogulskith@gmail.com>
2025-06-29 11:13:10 +0200

    letter setter wip + fix screen flasher + polish interaction enabler

--- Scenes/Inventory/item_memo.gd
+++ Scenes/Inventory/item_memo.gd
@@ -4,3 +4,4 @@ extends Resource
 @export var name: String
 @export var sprite: Texture2D
 @export var unlocked_at_start: bool
+@export var letter: String
---
+++ Scenes/Letter/letter_checker.gd
@@ -0,0 +1,30 @@
+class_name LetterChecker
+extends Control
+
+
+@export var letters: Array[LetterSetter]
+
+
+var done: bool
+
+
+func _ready() -> void:
+	for letter in letters:
+		letter.correct_set.connect(_on_correct_set)
+
+
+func _on_correct_set(value: bool) -> void:
+	if !done: return
+	if !value: return
+	if !_check_letters(): return
+	done = true
+	var tween = get_tree().create_tween()
+	modulate = Color.TRANSPARENT
+	tween.tween_property(self, "modulate", Color.WHITE, .5)
+	tween.play()
+
+
+func _check_letters() -> bool:
+	for letter in letters:
+		if !letter.is_correct: return false
+	return true
