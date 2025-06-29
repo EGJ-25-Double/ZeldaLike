f069a4f453d07b87abffe147491f1a16eec68f94
Shashamisen <shashamisen@gmail.com>
2025-06-29 13:19:58 +0200

    tentative intégration son changement de salle

--- Scenes/Entities/door.gd
+++ Scenes/Entities/door.gd
@@ -1,8 +1,10 @@
 extends Area2D
 
 @onready var spawn_point: Marker2D = $SpawnPoint
+@onready var room_change: AudioStreamPlayer = $"../RoomChange"
 
 func _on_body_entered(body: Node2D) -> void:
+	room_change.play()
 	body.global_position = spawn_point.global_position
 	var doggo = Doggo.instance
 	if (doggo != null):
