8231c9d18c1b3a6bc33696de3d3924b04a86cb98
Thomas Rogulski <rogulskith@gmail.com>
2025-06-29 02:22:33 +0200

    automatically equip new unlock item if a slot is empty

--- Scenes/Inventory/equipped_items.gd
+++ Scenes/Inventory/equipped_items.gd
@@ -8,10 +8,12 @@ extends Control
 func _ready() -> void:
 	InventoryUtils.subscribe_to_item_a_equipped(_on_item_a_equipped)
 	InventoryUtils.subscribe_to_item_b_equipped(_on_item_b_equipped)
+	InventoryUtils.subscribe_to_item_unlocked(_on_item_unlocked)
 
 func _exit_tree() -> void:
 	InventoryUtils.unsubscribe_from_item_a_equipped(_on_item_a_equipped)
 	InventoryUtils.unsubscribe_from_item_b_equipped(_on_item_b_equipped)
+	InventoryUtils.unsubscribe_from_item_unlocked(_on_item_unlocked)
 
 func _on_item_a_equipped(item: ItemMemo) -> void:
 	_on_item_equipped(item, item_a)
@@ -23,3 +25,10 @@ func _on_item_equipped(item: ItemMemo, target: EquippedItem) -> void:
 	var memo = item if item != null else no_item
 	target.ref.item = memo
 	target.ref.unlocked = true
+
+func _on_item_unlocked(item: ItemMemo) -> void:
+	if item == null: return
+	if item_a.ref.item == no_item:
+		_on_item_a_equipped(item)
+	elif item_b.ref.item == no_item:
+		_on_item_b_equipped(item)
