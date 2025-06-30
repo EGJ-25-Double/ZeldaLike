7e16874790b629db31d0eb4a23c59d3d888715be
lfatton <lfatton@LAPTOP-5LDR2FI6>
2025-06-30 14:02:09 +0200

    some final fixes

--- Scenes/Letter/letter_checker.gd
+++ Scenes/Letter/letter_checker.gd
@@ -21,7 +21,6 @@ func _ready() -> void:
 func _on_win_area_entered(body) -> void:
 	if !done: return
 	get_tree().change_scene_to_file("res://Scenes/Menus/victory_screen.tscn")
-	#TODO: WIN HERE
 
 
 func _on_correct_set(value: bool) -> void:
