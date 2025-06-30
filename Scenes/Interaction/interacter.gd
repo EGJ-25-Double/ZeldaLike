16c7c2b08fd5a76103b38fb5b758c6005a89d479
Thomas Rogulski <rogulskith@gmail.com>
2025-06-30 14:19:26 +0200

    no it is really fixed, yeah!

--- Scenes/Interaction/interacter.gd
+++ Scenes/Interaction/interacter.gd
@@ -9,12 +9,12 @@ func _process(delta: float) -> void:
 	
 	if InventoryUtils.is_inventory_open || timer > 0: return
 	if Input.is_action_just_pressed("action_a"):
-		timer = .5
+		timer = .4
 		InventoryUtils.use_item_a()
 		if (PlayerHero.instance != null && InventoryUtils.item_a != null):
 			PlayerHero.instance.start_interact_anim()
 	elif Input.is_action_just_pressed("action_b"):
-		timer = .5
+		timer = .4
 		InventoryUtils.use_item_b()
 		if (PlayerHero.instance != null && InventoryUtils.item_b != null):
 			PlayerHero.instance.start_interact_anim()
